81df040b1b1ce7518016fda51f21aeaf367c21f0
WernerTurban <wernerturban1@gmail.com>
2017-09-28 11:30:57 +0300

    Update forecastDaysTest.java

--- forecastDaysTest.java
+++ forecastDaysTest.java
@@ -2,9 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Created by Shelative on 27.09.2017.
- */
+
 class forecastDaysTest {
     @Test
     public void weatherForecastByDays() {
