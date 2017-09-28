b343da4accc42ae1d87dd86b4fc0241e624817f6
WernerTurban <wernerturban1@gmail.com>
2017-09-28 11:31:17 +0300

    Update todaysWeatherTest.java

--- todaysWeatherTest.java
+++ todaysWeatherTest.java
@@ -2,9 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Created by Shelative on 27.09.2017.
- */
+
 class todaysWeatherTest {
     @Test
     public void testWeatherByCity() {
